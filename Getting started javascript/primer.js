1b17548ae238e8ab1a64fef7008c9f4f520b7312
Sholesi Kofoworola <kofoworola.sholesi@cottacush.com>
2020-07-17 02:54:14 +0100

    primer javascript task

---
+++ Getting started javascript/primer.js
@@ -0,0 +1,23 @@
+let favoriteBooks = []
+const GREAT = 'great';
+
+function addFavoriteBook(bookName) {
+    let lowerCaseBookName = bookName.toLowerCase();
+    if (!lowerCaseBookName.includes(GREAT)) {
+        favoriteBooks.push(bookName);
+    }
+}
+
+function printFavoriteBooks() {
+    let number = favoriteBooks.length;
+    console.log(`My ${number} Favorite Books: ...`);
+    for (let book of favoriteBooks) {
+        console.log(book);
+    }
+}
+
+addFavoriteBook('The song of ice and Fire');
+addFavoriteBook('Vampire Kiss');
+addFavoriteBook('Harry Potter');
+
+printFavoriteBooks();
